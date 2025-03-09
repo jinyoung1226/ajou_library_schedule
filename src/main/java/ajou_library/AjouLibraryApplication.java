b1543df6641d8ef871bdeb8ef8346986fb89e6af
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-09 17:20:05 +0900

    chore: 아주대학교 중앙도서관 근로학생 스케줄 조정 프로젝트 설정

---
+++ src/main/java/ajou_library/AjouLibraryApplication.java
@@ -0,0 +1,13 @@
+package ajou_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjouLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjouLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ajou_library/AjouLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ajou_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjouLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
