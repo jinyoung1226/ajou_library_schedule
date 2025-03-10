49ba9a07f9d8b774fcc764d38943584d7460254a
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-10 17:20:14 +0900

    feat: 근로 시간 id가 없을 시, 처리하는 LectureScheduleNotFoundException 예외 처리 생성

---
+++ src/main/java/ajou_library/domain/LectureSchedule/exception/LectureScheduleNotFoundException.java
@@ -0,0 +1,7 @@
+package ajou_library.domain.LectureSchedule.exception;
+
+public class LectureScheduleNotFoundException extends RuntimeException {
+    public LectureScheduleNotFoundException(String message) {
+        super(message);
+    }
+}
