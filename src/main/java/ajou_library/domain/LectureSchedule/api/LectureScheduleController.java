dcd7fed648efa45ca9d354a9f91bcd528df03545
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-11 13:38:48 +0900

    fix: 생성자 주입 수정

--- src/main/java/ajou_library/domain/LectureSchedule/api/LectureScheduleController.java
+++ src/main/java/ajou_library/domain/LectureSchedule/api/LectureScheduleController.java
@@ -3,18 +3,16 @@ package ajou_library.domain.LectureSchedule.api;
 import ajou_library.domain.LectureSchedule.application.LectureScheduleService;
 import ajou_library.domain.LectureSchedule.dto.LectureScheduleDTO;
 import ajou_library.global.dto.BaseResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/lecture")
+@RequiredArgsConstructor
 public class LectureScheduleController {
 
     private final LectureScheduleService lectureScheduleService;
-
-    public LectureScheduleController(LectureScheduleService lectureScheduleService) {
-        this.lectureScheduleService = lectureScheduleService;
-    }
 
     @PostMapping("/{id}")
     public ResponseEntity<BaseResponse> addLectureSchedule(@PathVariable Long id, @RequestBody LectureScheduleDTO lectureScheduleDTO) {
