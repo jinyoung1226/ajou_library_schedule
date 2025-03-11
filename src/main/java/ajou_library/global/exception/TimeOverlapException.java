29b85d7cdf9ef45b4bad11b1554b16f927456a6a
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-11 13:33:41 +0900

    feat: TimeOverlapException 시간 중복 예외 클래스 추가

---
+++ src/main/java/ajou_library/global/exception/TimeOverlapException.java
@@ -0,0 +1,7 @@
+package ajou_library.global.exception;
+
+public class TimeOverlapException extends RuntimeException {
+    public TimeOverlapException(String message) {
+        super(message);
+    }
+}
