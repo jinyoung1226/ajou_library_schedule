0cb218ba9916e6af942d15d2f16c9d230001f319
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-11 13:57:57 +0900

    feat: LectureScheduleNotFoundException, WorkScheduleNotFoundException, 전역 예외 핸들러에 추가

--- src/main/java/ajou_library/global/GlobalExceptionHandler.java
+++ src/main/java/ajou_library/global/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package ajou_library.global;
 
+import ajou_library.domain.LectureSchedule.exception.LectureScheduleNotFoundException;
 import ajou_library.domain.User.exception.UserNotFoundException;
+import ajou_library.domain.WorkSchedule.exception.WorkScheduleNotFoundException;
 import ajou_library.global.dto.ErrorResponse;
 import ajou_library.global.exception.TimeOverlapException;
 import org.springframework.http.HttpStatus;
@@ -20,6 +22,20 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponse.builder().message(ex.getMessage()).build());
+    }
+
+    @ExceptionHandler(LectureScheduleNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleLectureScheduleNotFoundException(LectureScheduleNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponse.builder().message(ex.getMessage()).build());
+    }
+
+    @ExceptionHandler(WorkScheduleNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleWorkScheduleNotFoundException(WorkScheduleNotFoundException ex) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(ErrorResponse.builder().message(ex.getMessage()).build());
