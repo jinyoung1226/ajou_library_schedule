a643816a720c8a1281f113a350806fca6b106349
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-11 13:49:05 +0900

    feat: UserNotFoundException 전역 예외 핸들러에 추가

--- src/main/java/ajou_library/global/GlobalExceptionHandler.java
+++ src/main/java/ajou_library/global/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ajou_library.global;
 
+import ajou_library.domain.User.exception.UserNotFoundException;
 import ajou_library.global.dto.ErrorResponse;
 import ajou_library.global.exception.TimeOverlapException;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleTimeOverlapException(TimeOverlapException ex) {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
+                .body(ErrorResponse.builder().message(ex.getMessage()).build());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
                 .body(ErrorResponse.builder().message(ex.getMessage()).build());
     }
 }
