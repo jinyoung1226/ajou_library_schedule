d8f43f562f0f899e37598f6c417a56bcbcdf4b20
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-11 13:34:12 +0900

    feat: 근로 시간 등록(기존 근로 시간 중복 방지까지 처리) 및 삭제 서비스 로직 구현

--- src/main/java/ajou_library/domain/LectureSchedule/application/LectureScheduleService.java
+++ src/main/java/ajou_library/domain/LectureSchedule/application/LectureScheduleService.java
@@ -8,9 +8,13 @@ import ajou_library.domain.User.entity.User;
 import ajou_library.domain.User.exception.UserNotFoundException;
 import ajou_library.domain.User.repository.UserRepository;
 import ajou_library.global.dto.BaseResponse;
+import ajou_library.global.exception.TimeOverlapException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +35,24 @@ public class LectureScheduleService {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException("근로학생을 찾을 수 없습니다."));
 
+        // 해당 유저의 요일에 이미 등록된 근로 시간을 전부 리스트로 갖고와서 중복인지 대조하는 과정
+        List<LectureSchedule> lectureSchedules = lectureScheduleRepository.findByUserAndDay(user, lectureScheduleDTO.getDay());
+        boolean conflict = false;
+
+        for (LectureSchedule lectureSchedule : lectureSchedules) {
+            boolean result = checkTimeConflict(lectureSchedule.getStartTime(), lectureSchedule.getEndTime(), lectureScheduleDTO.getStartTime(), lectureScheduleDTO.getEndTime());
+            if (result) {
+                conflict = true;
+            }
+        }
+
+        // 중복 시, 시간 중복 예의 발생
+        if(conflict) {
+            log.info("시간이 중복됩니다.");
+            throw new TimeOverlapException("시간이 중복됩니다.");
+        }
+
+        // 중복이 아닐 시, 근로 시간 저장
         LectureSchedule lectureSchedule = LectureSchedule.builder()
                 .startTime(lectureScheduleDTO.getStartTime())
                 .endTime(lectureScheduleDTO.getEndTime())
@@ -39,12 +61,17 @@ public class LectureScheduleService {
                 .build();
         lectureScheduleRepository.save(lectureSchedule);
 
-        // 같은 근로 시간에 또 같은 시간을 넣는 지 - 즉 중복인지 확인하는 로직 추가 필요
+        log.info("저장된 근로 시간 정보 " + lectureSchedule.getDay() + " 시작 시간: " + lectureSchedule.getStartTime() + " 종료 시간: " + lectureSchedule.getEndTime());
+
         // 추후에 학교 수업이랑도 겹치는지 확인 필요
 
         return BaseResponse.builder()
                 .message("근로 시간 등록이 완료되었습니다.")
                 .build();
+    }
+
+    private boolean checkTimeConflict(LocalTime existStartTime, LocalTime existEndTime, LocalTime newStartTime, LocalTime newEndTime) {
+        return newStartTime.isBefore(existEndTime) && newEndTime.isAfter(existStartTime);
     }
 
     /**
--- src/main/java/ajou_library/domain/LectureSchedule/repository/LectureScheduleRepository.java
+++ src/main/java/ajou_library/domain/LectureSchedule/repository/LectureScheduleRepository.java
@@ -1,8 +1,13 @@
 package ajou_library.domain.LectureSchedule.repository;
 
 import ajou_library.domain.LectureSchedule.entity.LectureSchedule;
+import ajou_library.domain.User.entity.User;
+import ajou_library.global.entity.DayOfWeek;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface LectureScheduleRepository extends JpaRepository<LectureSchedule, Long> {
 
+    List<LectureSchedule> findByUserAndDay(User user, DayOfWeek day);
 }
